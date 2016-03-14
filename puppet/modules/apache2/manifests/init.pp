34553c68c368222ea7f038a0664618d15524e8f8
James Fehr <jamiefehr@fastmail.es>
2016-03-14 17:04:07 +0100

    apache is now working for vhosts the way I want it to

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -1,31 +1,58 @@
 class apache2::install {
-  package { "apache2":
+
+  package { 'apache2':
     ensure  => latest,
-    require => Exec["apt-update"],
+    require => Exec['apt-update'],
   }
 
-  service { "apache2":
-    ensure  => "running",
+  service { 'apache2':
+    ensure  => 'running',
     require => [
-      Package["apache2"],
-      File["/etc/apache2/apache2.conf"],
+      Package['apache2'],
+      File['vhosts.conf'],
     ]
   }
 
-  file { "/etc/apache2/apache2.conf":
-    ensure => present,
+  file { 'apache2.conf':
+    name => '/etc/apache2/apache2.conf',
+    ensure => 'present',
     owner => 'root',
     group => 'root',
     mode => 644,
-    source => "puppet:///modules/apache2/apache2.conf",
-    require => Package["apache2"],
-    notify => Service["apache2"],
+    source => 'puppet:///modules/apache2/apache2.conf',
+    require => Package['apache2'],
+    notify => Service['apache2'],
   }
+
+  file { 'vhosts.conf':
+    name => '/etc/apache2/sites-enabled/vhosts.conf',
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    source => '/vagrant/vhosts.conf',
+    require => Package['apache2'],
+    notify => Service['apache2'],
+  }
+
+  file { '/srv/log':
+    ensure => 'directory',
+    # owner => 'root',
+    # group => 'root',
+    mode => 755,
+  }
+
+  apache2::loadmodule{ 'rewrite': }
+
 }
 
-# define apache2::loadmodule () {
-#      exec { "/usr/sbin/a2enmod $name" :
-#           unless => "/bin/readlink -e /etc/apache2/mods-enabled/${name}.load",
-#           notify => Service[apache2]
-#      }
-# }
+# TODO: Apache2 loadmodule
+# https://snowulf.com/2012/04/05/puppet-quick-tip-enabling-an-apache-module/
+# https://docs.puppetlabs.com/puppet/4.3/reference/lang_defined_types.html
+define apache2::loadmodule () {
+     exec { '/usr/sbin/a2enmod $name' :
+          unless => '/bin/readlink -e /etc/apache2/mods-enabled/${name}.load',
+          notify => Service['apache2'],
+          require => Package ['apache2'],
+     }
+}
